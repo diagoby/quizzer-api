99f26503375a9eb53373bda85ed3d825cdebe67f
Vladislav Hutych <mandarineye@gmail.com>
2021-04-25 19:23:47 +0300

    fix: Remove import of deleted package in IUserService

--- src/main/java/com/orsoft/quizzer_api/domain/services/IUserService.java
+++ src/main/java/com/orsoft/quizzer_api/domain/services/IUserService.java
@@ -3,10 +3,7 @@ package com.orsoft.quizzer_api.domain.services;
 import com.orsoft.quizzer_api.domain.contracts.dto.LoginUserDTO;
 import com.orsoft.quizzer_api.domain.contracts.dto.ReadUserDTO;
 import com.orsoft.quizzer_api.domain.contracts.dto.RegisterUserDTO;
-import com.orsoft.quizzer_api.domain.errors.UserAlreadyRegisteredError;
 import com.orsoft.quizzer_api.domain.utils.Result;
-
-import java.util.Optional;
 
 public interface IUserService {
   Result<Object, String> register(RegisterUserDTO userDto);
