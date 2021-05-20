7854e0f36354bc07fe99d88a998806dfd250bebc
Vladislav Hutych <mandarineye@gmail.com>
2021-05-20 19:06:38 +0300

    feat(services): Implement create quiz method

--- src/main/java/com/orsoft/quizzer_api/domain/services/quiz/QuizService.java
+++ src/main/java/com/orsoft/quizzer_api/domain/services/quiz/QuizService.java
@@ -1,9 +1,14 @@
 package com.orsoft.quizzer_api.domain.services.quiz;
 
+import com.orsoft.quizzer_api.domain.contracts.dto.quiz.CreateQuizDTO;
 import com.orsoft.quizzer_api.domain.contracts.dto.quiz.ReadQuizDTO;
+import com.orsoft.quizzer_api.domain.contracts.mappers.QuestionMapper;
 import com.orsoft.quizzer_api.domain.contracts.mappers.QuizMapper;
+import com.orsoft.quizzer_api.domain.models.quiz.Quiz;
+import com.orsoft.quizzer_api.domain.models.user.User;
 import com.orsoft.quizzer_api.domain.utils.Result;
 import com.orsoft.quizzer_api.infrastructure.repositories.IQuizRepository;
+import com.orsoft.quizzer_api.infrastructure.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,13 +21,32 @@ public class QuizService implements IQuizService {
   private IQuizRepository quizRepository;
 
   @Autowired
+  private IUserRepository userRepository;
+
+  @Autowired
   private QuizMapper quizMapper;
   
   @Override
   public Result<ReadQuizDTO, String> getQuizById(String id) {
     Optional<ReadQuizDTO> readQuizDto = quizRepository.findById(UUID.fromString(id))
-      .map(this.quizMapper::toDTO);
+      .map(quizMapper::toDTO);
 
     return Result.ofOptional(readQuizDto, String.format("No quiz found by id \"%s\"", id));
+  }
+
+  @Override
+  public Result<Object, String> createQuiz(CreateQuizDTO quizDto) {
+    Optional<User> userEntity = userRepository.findById(UUID.fromString(quizDto.creatorId));
+
+    if(userEntity.isEmpty()) {
+      return Result.error(String.format("No user found with id \"%s\"", quizDto.creatorId));
+    }
+
+    Quiz quizEntity = quizMapper.toEntity(quizDto);
+    quizEntity.setUser(userEntity.get());
+
+    quizRepository.save(quizEntity);
+
+    return Result.empty();
   }
 }
