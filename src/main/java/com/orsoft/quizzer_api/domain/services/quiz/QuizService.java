ae6186863406b53637191d5b0ecd4000009ffb29
Vladislav Hutych <mandarineye@gmail.com>
2021-05-24 00:01:33 +0300

    feat(quizzes): Implement retrieving user quizzes list

--- src/main/java/com/orsoft/quizzer_api/domain/services/quiz/QuizService.java
+++ src/main/java/com/orsoft/quizzer_api/domain/services/quiz/QuizService.java
@@ -2,7 +2,6 @@ package com.orsoft.quizzer_api.domain.services.quiz;
 
 import com.orsoft.quizzer_api.domain.contracts.dto.quiz.CreateQuizDTO;
 import com.orsoft.quizzer_api.domain.contracts.dto.quiz.ReadQuizDTO;
-import com.orsoft.quizzer_api.domain.contracts.mappers.QuestionMapper;
 import com.orsoft.quizzer_api.domain.contracts.mappers.QuizMapper;
 import com.orsoft.quizzer_api.domain.models.quiz.Quiz;
 import com.orsoft.quizzer_api.domain.models.user.User;
@@ -13,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class QuizService implements IQuizService {
@@ -25,14 +26,6 @@ public class QuizService implements IQuizService {
 
   @Autowired
   private QuizMapper quizMapper;
-  
-  @Override
-  public Result<ReadQuizDTO, String> getQuizById(String id) {
-    Optional<ReadQuizDTO> readQuizDto = quizRepository.findById(UUID.fromString(id))
-      .map(quizMapper::toDTO);
-
-    return Result.ofOptional(readQuizDto, String.format("No quiz found by id \"%s\"", id));
-  }
 
   @Override
   public Result<Object, String> createQuiz(CreateQuizDTO quizDto) {
@@ -48,5 +41,28 @@ public class QuizService implements IQuizService {
     quizRepository.save(quizEntity);
 
     return Result.empty();
+  }
+
+  @Override
+  public Result<ReadQuizDTO, String> getQuizById(String id) {
+    Optional<ReadQuizDTO> readQuizDto = quizRepository.findById(UUID.fromString(id))
+      .map(quizMapper::toDTO);
+
+    return Result.ofOptional(readQuizDto, String.format("No quiz found by id \"%s\"", id));
+  }
+
+  @Override
+  public Result<Set<ReadQuizDTO>, String> getUserQuizzes(String userId) {
+    Optional<User> userEntity = userRepository.findById(UUID.fromString(userId));
+
+    if(userEntity.isEmpty()) {
+      return Result.error(String.format("No user found with id \"%s\"", userId));
+    }
+
+    return Result.ok(
+      userEntity.get().getQuizzes().stream()
+        .map(quizMapper::toDTO)
+        .collect(Collectors.toSet())
+    );
   }
 }
--- src/main/java/com/orsoft/quizzer_api/infrastructure/controllers/UserController.java
+++ src/main/java/com/orsoft/quizzer_api/infrastructure/controllers/UserController.java
@@ -1,10 +1,18 @@
 package com.orsoft.quizzer_api.infrastructure.controllers;
 
 
+import com.orsoft.quizzer_api.domain.contracts.dto.quiz.ReadQuizDTO;
+import com.orsoft.quizzer_api.domain.services.quiz.IQuizService;
 import com.orsoft.quizzer_api.domain.services.user.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Set;
 
 @RestController
 @RequestMapping(path = "/users")
@@ -12,5 +20,13 @@ public class UserController {
   @Autowired
   IUserService userService;
 
-  // TODO: Implement
+  @Autowired
+  private IQuizService quizService;
+
+  @GetMapping("{id}/quizzes")
+  public Set<ReadQuizDTO> getUserQuizzes(@PathVariable("id") String userId) {
+    return quizService.getUserQuizzes(userId).getOrThrow((error) ->
+      new ResponseStatusException(HttpStatus.NOT_FOUND, error)
+    );
+  }
 }
